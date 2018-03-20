7c0fc980a97a6d63584436ecc0e7d807e3269de1
Kamil Chęć <k.chec.prv@gmail.com>
2018-03-20 18:46:34 +0100

    Stworzenie projektu i klasy StartMenu

---
+++ src/pl/edu/pw/fizyka/pojava/ArcherTheGame/StartMenu.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.fizyka.pojava.ArcherTheGame;
+
+import javax.swing.JFrame;
+
+public class StartMenu extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
